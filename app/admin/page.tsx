3696efae4848293fa64b615a0bd12eb773712574
Keshav Pawar <keshavpawar784@gmail.com>
2025-12-22 14:16:28 +0530

    Added Sales by Location card with country wise percentage bar

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,7 +10,7 @@ export default function AdminPage() {
 
                 <div className="flex flex-row ml-100 p-2">
                     <label htmlFor="text" className="font-semibold p-2">Search</label>
-                    <input type="text" placeholder="Search here..." className="border-1 border-black-300 rounded-lg w-67 p-2 focus:border-white-500 focus:ring-2 focus:ring-white-500 focus:outline-none" />
+                    <input type="text" placeholder="Search here..." className="border-1 border-white-300 rounded-lg w-67 p-2 focus:border-white-500 focus:ring-2 focus:ring-white-500 focus:outline-none" />
                 </div>
             </nav>
             <div className="flex justify-evenly relative right-8 rounded-3xl group p-6">
@@ -34,8 +34,8 @@ export default function AdminPage() {
 
             </div>
 
-            <div className="flex justify-center relative top-50 mt-6 overflow-y-auto bg-white rounded-lg shadow">
-                <table className="w-full border-collapse">
+            <div className="flex justify-start mt-60 overflow-y-auto bg-white rounded-lg shadow">
+                <table className=" w-200 border-collapse border border-2-grey ml-20">
                     <thead className="bg-blue-200 rounded-lg">
                         <tr>
                             <th className="text-xl font-semibold text-black-600 border-b">Recent Users</th>
@@ -77,6 +77,32 @@ export default function AdminPage() {
                         </tr>
                     </tbody>
                 </table>
+                <div className="border border-black-500 w-70 h-92 ml-4 rounded-lg p-2 shadow-xl">
+                    <h3 className="bg-blue-200 text-xl font-semibold text-center rounded-lg">Sales By Location</h3>
+                    <img src="/Earth.jpeg" alt="Earth Image" className="h-45 w-70 rounded-3xl py-2" />
+                    <div className="w-full mt-3">
+                        <div className="flex justify-between mb-1">
+                            <span className="font-medium text-sm">India</span>
+                            <span className="text-sm font-medium">70%</span>
+                        </div>
+                        <div className="bg-blue-500 h-2 rounded-full w-[70%]"></div>
+                        <div className="flex justify-between mb-1">
+                            <span className="font-medium text-sm">Australia</span>
+                            <span className="text-sm font-medium">30%</span>
+                        </div>
+                        <div className="bg-blue-500 h-2 rounded-full w-[30%]"></div>
+                        <div className="flex justify-between mb-1">
+                            <span className="font-medium text-sm">New York</span>
+                            <span className="font-medium text-smm">45%</span>
+                        </div>
+                        <div className="bg-blue-500 h-2 rounded-full w-[45%]"></div>
+                        <div className="flex justify-between mb-1">
+                            <span className="font-medium text-sm">USA</span>
+                            <span className="font-medium text-sm">80%</span>
+                        </div>
+                        <div className="bg-blue-500 h-2 rounded-full w-[80%]"></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
