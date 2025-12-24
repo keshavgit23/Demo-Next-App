4c0e6935f9671778449bfabd5835b92d6a2aa513
Keshav Pawar <keshavpawar784@gmail.com>
2025-12-24 21:24:57 +0530

    Added total sales card UI

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,11 +8,11 @@ export default function AdminPage() {
 
         <>
             <AdminNavbar />
-            <SmallCard/>
+            <SmallCard />
             <div>
 
-                <div className="flex justify-start mt-60 overflow-y-auto bg-white rounded-lg shadow">
-                    <table className=" w-200 border-collapse border border-2-grey ml-20">
+                <div className="flex justify-start mt-40 overflow-y-auto bg-white rounded-lg shadow">
+                    <table className=" w-180 border-collapse border border-2-grey ml-22">
                         <thead className="bg-blue-200 rounded-lg">
                             <tr>
                                 <th className="text-xl font-semibold text-black-600 border-b">Recent Users</th>
@@ -80,49 +80,18 @@ export default function AdminPage() {
                             <div className="bg-blue-500 h-2 rounded-full w-[80%]"></div>
                         </div>
                     </div>
-
                     <div className="border border-black-500 w-70 h-92 ml-4 rounded-lg p-2 shadow-xl">
                         <h3 className="bg-blue-200 text-xl font-semibold text-center rounded-lg">Total Sales</h3>
-                        <div className="w-full mt-3">
-                            <div className="flex justify-between mb-1">
-                                <ul className="">
-                                    <span className="font-medium text-sm">
-                                        <li>Direct</li></span>
-                                    <span className="text-sm font-medium">
-                                        <li>4000/-</li>
-                                    </span>
-                                </ul>
-                            </div>
+                        <div className="relative w-40 h-40 mx-auto mt-4 rounded-full bg-[conic-gradient(#2563eb_0deg_144deg,#22c55e_144deg_252deg,#f59e0b_252deg_324deg,#ef4444_324deg_360deg)]">
+                            <div className="absolute inset-6 bg-white rounded-full flex items-center justify-center text-sm font-semibold"></div>
                         </div>
-                        <div className="flex justify-between mb-1">
-                            <ul>
-                                <span className="font-medium text-sm">
-                                    <li>Affiliate</li>
-                                </span>
-                                <span className="text-sm font-medium">
-                                    <li>3000/-</li>
-                                </span>
-                            </ul>
-                        </div>
-                        <div className="flex justify-between mb-1">
-                            <ul>
-                                <span className="font-medium text-sm">
-                                    <li>Sponsored</li>
-                                </span>
-                                <span className="font-medium text-sm">
-                                    <li>2000/-</li>
-                                </span>
-                            </ul>
-                        </div>
-                        <div className="flex justify-between mb-1">
-                            <ul>
-                                <span className="font-medium text-sm">
-                                    <li>E-mail</li>
-                                </span>
-                                <span className="font-medium text-sm">
-                                    <li>1000/-</li>
-                                </span>
-                            </ul>
+
+
+                        <div className="mt-4 space-y-1 text-sm p-2">
+                            <p><span className="inline-block w-3 h-3 bg-blue-600 mr-2"></span>Direct <span className="ml-30">1000rs</span></p>
+                            <p><span className="inline-block w-3 h-3 bg-green-500 mr-2"></span>Affiliate <span className="ml-28">3000rs</span></p>
+                            <p><span className="inline-block w-3 h-3 bg-yellow-500 mr-2"></span>Sponsored <span className="ml-23">2000rs</span></p>
+                            <p><span className="inline-block w-3 h-3 bg-red-500 mr-2"></span>E-mail <span className="ml-30">1000rs</span></p>
                         </div>
                     </div>
                 </div>
