4ebe84b55f30f2d179fef49db2c8eb11a840d12b
Keshav Pawar <keshavpawar784@gmail.com>
2025-12-25 22:08:56 +0530

    Updated: SmallCard layout

--- app/components/SmallCard.tsx
+++ app/components/SmallCard.tsx
@@ -2,20 +2,20 @@ export default function SmallCard(){
     return (
 
         <div className="flex justify-evenly relative right-8 rounded-3xl group p-6">
-                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-30 left-10 rounded-lg  bg-white p-4 hover:bg-blue-100 tansition">
+                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-10 left-10 rounded-lg  bg-white p-4 hover:bg-blue-100 tansition">
                         <h1 className="text-xl font-semibold mt-5">Total Sales</h1>
                         <p className="mt-20 absolute text-xl">10000</p>
                     </div>
 
-                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-30 left-10 rounded-lg p-4 bg-white hover:bg-blue-100 tansition">
+                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-10 left-10 rounded-lg p-4 bg-white hover:bg-blue-100 tansition">
                         <h1 className="text-xl font-semibold mt-5">Total Orders</h1>
                         <p className="mt-20 absolute text-xl">500+</p>
                     </div>
-                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-30 left-10 rounded-lg p-4 bg-white hover:bg-blue-100 tansition">
+                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-10 left-10 rounded-lg p-4 bg-white hover:bg-blue-100 tansition">
                         <h1 className="text-xl font-semibold mt-5">Revenue</h1>
                         <p className="mt-20 absolute text-xl">10000+ rs</p>
                     </div>
-                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-30 left-10 rounded-lg p-4 bg-white hover:bg-blue-100 tansition">
+                    <div className="border-4 border-blue-500 w-70 h-50 flex justify-center relative top-10 left-10 rounded-lg p-4 bg-white hover:bg-blue-100 tansition">
                         <h1 className="text-xl font-semibold mt-5">Pending/Reports</h1>
                         <p className="mt-20 absolute text-xl">150</p>
                     </div>
